4a59f01364e8bdabe4423bde0d2319e130af4104
jeremyrice98 <ricefamily1003@gmail.com>
2020-06-09 11:40:29 -0600

    more list features

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,41 +1,8 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-    var response = fetch("https://api.github.com/users/octocat/repos");
-    console.log(response);
-};
-
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
-
-        fetch(apiUrl)
-            .then(function(response) {
-                // request was successful
-                if (response.ok) {
-                    response.json().then(function(data) {
-                        displayRepos(data, user);
-                    });
-                } else {
-                    alert("Error: " + response.statusText);
-                }
-            })
-            .catch(function(error) {
-                // Notice this `.catch()` getting chained onto the end of the `.then()` method
-                alert("Unable to connect to GitHub");
-            });
-    });
-
-
-};
-
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -48,7 +15,27 @@ var formSubmitHandler = function(event) {
     } else {
         alert("Please enter a GitHub username");
     }
-    console.log(event);
+};
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to GitHub");
+        });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -66,9 +53,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
@@ -91,14 +78,34 @@ var displayRepos = function(repos, searchTerm) {
         // append to container
         repoEl.appendChild(statusEl);
 
-        // append container to the dom
+        // append container to the DOM
         repoContainerEl.appendChild(repoEl);
     }
-    console.log(repos);
-    console.log(searchTerm);
 };
 
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
